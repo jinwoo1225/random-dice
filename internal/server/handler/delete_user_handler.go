490f16ec7a087f656463c4ec2f7c6f8a0e4f8cb2
jinwoo1225 <jinwoo17962@icloud.com>
2022-11-21 22:45:20 +0900

    feat: add delte user handler

---
+++ internal/server/handler/delete_user_handler.go
@@ -0,0 +1,73 @@
+package handler
+
+import (
+	"context"
+	"errors"
+	"github.com/benbjohnson/clock"
+	"github.com/jinwoo1225/random-dice/client"
+	randomdicev1 "github.com/jinwoo1225/random-dice/gen/proto/go/randomdice/v1"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type DeleteUserFunc func(ctx context.Context, req *randomdicev1.DeleteUserRequest) (*randomdicev1.DeleteUserResponse, error)
+
+func DeleteUser(
+	clk clock.Clock,
+	mdb client.MongoDBClient,
+) DeleteUserFunc {
+	return func(ctx context.Context, req *randomdicev1.DeleteUserRequest) (*randomdicev1.DeleteUserResponse, error) {
+		if err := validateDeleteUserRequest(req); err != nil {
+			return nil, err
+		}
+
+		var user User
+
+		objectID, err := primitive.ObjectIDFromHex(req.Id)
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+
+		err = mdb.FindOne(ctx, MongoDatabaseName, MongoUserCollectionName, bson.M{"_id": objectID}).Decode(&user)
+		if err != nil && errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, status.Error(codes.NotFound, err.Error())
+		}
+
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+
+		res, err := mdb.UpdateOne(ctx, MongoDatabaseName, MongoUserCollectionName, bson.M{"_id": objectID}, bson.M{"$set": bson.M{"deleted_at": clk.Now()}})
+		if err != nil && errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, status.Error(codes.NotFound, err.Error())
+		}
+
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+
+		var deleted bool
+		if res.ModifiedCount > 0 {
+			deleted = true
+		}
+
+		return &randomdicev1.DeleteUserResponse{
+			Success: deleted,
+		}, nil
+	}
+}
+
+func validateDeleteUserRequest(req *randomdicev1.DeleteUserRequest) error {
+	if req.Id == "" {
+		return errInvalidID
+	}
+
+	if _, err := primitive.ObjectIDFromHex(req.Id); err != nil {
+		return errInvalidID
+	}
+
+	return nil
+}
